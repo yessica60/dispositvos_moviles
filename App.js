df6479b8ad0e1eeb8a4571b69b1bacb83cead9d9
yessica60 <84580770+yessica60@users.noreply.github.com>
2023-10-18 19:49:46 -0400

    first-commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Yessica María Villavizar Hernández #2019-1056</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
