61cfa1b38e586a9e314bbb5a2fa5a26f1cac393d
EdwardB11 <edwardbrito11@hotmail.com>
2023-10-24 12:56:46 -0400

    feat: add styles and add components in view

--- first-partial/App.tsx
+++ first-partial/App.tsx
@@ -1,23 +1,32 @@
 import React from "react";
-import { SafeAreaView, Text, View } from "react-native";
+import { SafeAreaView, ScrollView, View } from "react-native";
 import SearchBox from "./components/molecules/SearchBox";
-import CardList from "./components/organisms/CardList";
 import User from "./components/molecules/User";
+import CardList from "./components/organisms/CardList";
+import useGitHubData from "./hooks/useGitHubData";
+import TextAtom from "./components/atoms/Text";
 
 export default function App() {
-  const data = {
-    public_repos: 1,
-    followers: 1,
-    following: 1,
-  };
+  const { data, loading, search } = useGitHubData();
+
   return (
-    <SafeAreaView className="container bg-blue-900  flex-1 mx-auto ">
-      <SearchBox onSearch={() => {}} />
-            <User src="test.png" username="Edwardb11" />
-      <CardList data={data} />
-      <View>
-        <Text className="text-gray-50 text-center">Hello!</Text>
-      </View>
+    <SafeAreaView className="bg-blue-800 flex-1">
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        className="h-screen p-4 mx-auto">
+        <View>
+          <SearchBox onSearch={search} />
+          {loading && (
+            <TextAtom text="Loading..." key={1}/>
+          )}
+          {data && (
+            <View>
+              <User src={data.avatar_url} username={data.login} />
+              <CardList data={data} />
+            </View>
+          )}
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
