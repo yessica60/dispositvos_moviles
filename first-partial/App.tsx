91f4ee4fb75056a60c81ed7be9989756a8fe8baa
EdwardB11 <edwardbrito11@hotmail.com>
2023-10-24 12:30:28 -0400

    feat: add user in app

--- first-partial/App.tsx
+++ first-partial/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { SafeAreaView, Text, View } from "react-native";
 import SearchBox from "./components/molecules/SearchBox";
+import User from "./components/molecules/User";
 
 export default function App() {
 
   return (
     <SafeAreaView className="container bg-blue-900  flex-1 mx-auto ">
       <SearchBox onSearch={() => {}} />
+      <User src="test.png" username="Edwardb11" />
+
     <View>
       <Text className="text-gray-50 text-center">Hello!</Text>
     </View>
