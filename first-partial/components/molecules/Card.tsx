04316afe22485e760982d6d012207c3054ec74d3
yessica60 <84580770+yessica60@users.noreply.github.com>
2023-10-23 23:31:56 -0400

    feat: create component card

---
+++ first-partial/components/molecules/Card.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { View } from "react-native";
+import TextAtom from "../atoms/Text";
+
+type CardProps = {
+  number: string | number;
+  text: string;
+} & { [key: string]: any };
+
+const Card = ({ number, text, ...other }: CardProps) => {
+  return (
+    <View
+      className="flex justify-center items-center rounded-md w-36 h-36"
+      {...other}>
+      <View>
+        <TextAtom text={number} position="center" />
+        <TextAtom text={text} />
+      </View>
+    </View>
+  );
+};
+
+export default Card;
