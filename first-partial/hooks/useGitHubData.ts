9e6ddaeacac4fc2e2ff6442c12862504ae2ea9e6
EdwardB11 <edwardbrito11@hotmail.com>
2023-10-24 12:40:24 -0400

    feat: create custom hook for get de use in github in search

---
+++ first-partial/hooks/useGitHubData.ts
@@ -0,0 +1,38 @@
+import { useState, useCallback } from "react";
+
+export type Data = {
+  avatar_url: string;
+  followers: string | number;
+  following: string | number;
+  login: string;
+  public_repos: string | number;
+};
+
+const useGitHubData = () => {
+  const [data, setData] = useState<Data | null>(null);
+
+  const search = useCallback(async (searchTerm: string) => {
+    if (searchTerm === "") {
+      alert("Please enter something");
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        `https://api.github.com/users/${searchTerm}`
+      );
+      if (response.ok) {
+        const jsonData = await response.json();
+        setData(jsonData);
+      } else {
+        alert("User not found");
+      }
+    } catch (error) {
+      alert("An error occurred");
+    }
+  }, []);
+
+  return { data, search };
+};
+
+export default useGitHubData;
