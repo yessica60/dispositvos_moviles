7aa94b2596d215b363dadef42a9bfb8a34b130b5
yessica60 <84580770+yessica60@users.noreply.github.com>
2023-10-23 23:22:07 -0400

    feat: create component textinput

---
+++ first-partial/components/atoms/TextInput.tsx
@@ -0,0 +1,26 @@
+mport React, { useState } from 'react';
+import { TextInput } from 'react-native';
+
+type Props = {
+  placeholder: string;
+  onChangeText: (text: string) => void;
+}
+
+const TextInputAtom = ({ placeholder, onChangeText }: Props) => {
+  const [text, setText] = useState("");
+
+  return (
+    <TextInput
+      placeholder={placeholder}
+      onChangeText={(newText) => {
+        setText(newText);
+        onChangeText(newText);
+      }}
+      value={text}
+      className="border p-2 mx-4 w-2/3 border-gray-100 rounded text-gray-100 focus:border-2 focus:border-purple-500"
+      placeholderTextColor="#ffffff"
+    />
+  );
+}
+
+export default TextInputAtom;
