0e971d5710785d4dc2e3bc7deae5f154ee4885dc
EdwardB11 <edwardbrito11@hotmail.com>
2023-10-24 12:36:24 -0400

    feat: implements cardlist for data in card

--- first-partial/App.tsx
+++ first-partial/App.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { SafeAreaView, Text, View } from "react-native";
 import SearchBox from "./components/molecules/SearchBox";
+import CardList from "./components/organisms/CardList";
 
 export default function App() {
-
+  const data = {
+    public_repos: 1,
+    followers: 1,
+    following: 1,
+  };
   return (
     <SafeAreaView className="container bg-blue-900  flex-1 mx-auto ">
       <SearchBox onSearch={() => {}} />
-    <View>
-      <Text className="text-gray-50 text-center">Hello!</Text>
-    </View>
-  </SafeAreaView>
+      <CardList data={data} />
+      <View>
+        <Text className="text-gray-50 text-center">Hello!</Text>
+      </View>
+    </SafeAreaView>
   );
 }
